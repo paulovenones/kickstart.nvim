8c8631999c1448c8a697312c8a4a9e9800ddd6ca
paulovenones <paulo.venones@gmail.com>
2025-12-17 13:09:32 +0100

    add diagnostic list keymap

--- lua/paulovenones/remap.lua
+++ lua/paulovenones/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Diagnostics
+vim.keymap.set('n', '<leader>Q', vim.diagnostic.setqflist, { desc = 'Open diagnostic [Q]uickfix list (all buffers)' })
